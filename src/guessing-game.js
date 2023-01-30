8e8e60d36a124b933e86bbd97ed1c587d277cf6b
Malsim Dauhanosav <max_air@bk.ru>
2023-01-30 19:17:26 +0300

    done 30/01/2023

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.max - this.min) / 2 + this.min);
     }
 
     lower() {
-
+        this.max = this.guess();
+        return this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
+        return this.guess();
     }
 }
 
